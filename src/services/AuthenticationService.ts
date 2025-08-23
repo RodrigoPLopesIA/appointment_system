1878287f1838c3bd1b5b5e5d5a7dd3d3cb95a84d
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-23 14:06:53 -0300

    refactor: rewrite verify password and create a new method to find user by email

--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -1,27 +1,32 @@
 import { Repository } from "typeorm";
 import User from "../entity/User";
 import LoginDTO from "../dto/input/login.input";
-import { compareSync } from "bcrypt";
+import { compareSync, hashSync } from "bcrypt";
 import JWTService from "./JWTService";
 
 export default class AuthenticationService {
   constructor(private readonly userRepository: Repository<User>) {}
 
   public async login({ email, password }: LoginDTO) : Promise<string> {
-    const user = await this.userRepository.findOneBy({
-      email: email as string,
-    });
+    const user = await this.findUserByEmail(email as string)
 
-    if (!user || !this.verifyPassword(password as string, user.password))
-      throw new Error("Invalid credentials!");
-
+    this.verifyPassword(password, user.password)
+    
     // generate token
     const token = JWTService.generateToken({email: user.email, id: user.id})
     return token
   }
 
+  public async findUserByEmail(email: string) : Promise<User>{
+    const user = await this.userRepository.findOneBy({
+      email: email,
+    });
+    if(!user) throw new Error("Invalid credentials!")
+    return user
+  }
 
-  private verifyPassword(password, hashPassword) : boolean {
-    return compareSync(password, hashPassword)
+  public verifyPassword(password, hashPassword) : void {
+    if(!hashSync(password as string, hashPassword))
+      throw new Error("Invalid credentials!");
   }
 }
