eb2622475c3cc7de8a869edaa6d5fcd0dbd978c5
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-23 14:00:40 -0300

    feat: generate user token

--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -2,11 +2,12 @@ import { Repository } from "typeorm";
 import User from "../entity/User";
 import LoginDTO from "../dto/input/login.input";
 import { compareSync } from "bcrypt";
+import JWTService from "./JWTService";
 
 export default class AuthenticationService {
   constructor(private readonly userRepository: Repository<User>) {}
 
-  public async login({ email, password }: LoginDTO) {
+  public async login({ email, password }: LoginDTO) : Promise<string> {
     const user = await this.userRepository.findOneBy({
       email: email as string,
     });
@@ -15,8 +16,8 @@ export default class AuthenticationService {
       throw new Error("Invalid credentials!");
 
     // generate token
-
-    
+    const token = JWTService.generateToken({email: user.email, id: user.id})
+    return token
   }
 
 
