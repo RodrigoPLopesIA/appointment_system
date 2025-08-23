bb3207913c5ec48e09ca13456c5a86ebe0aab494
RodrigoPLopesIA <rodrigoplopes25@gmail.com>
2025-08-23 14:15:08 -0300

    refactor: remove getUser and getUsers queries

--- src/resolvers/User.resolver.ts
+++ src/resolvers/User.resolver.ts
@@ -5,23 +5,12 @@ import CreateUserDTO from "../dto/input/create-user.input";
 import UserEntity from "../entity/User";
 import { AppDataSource } from "../data-source";
 
-const userService = new UserService(AppDataSource.getRepository<UserEntity>(UserEntity));
+const userService = new UserService(
+  AppDataSource.getRepository<UserEntity>(UserEntity)
+);
 
 @Resolver()
 export default class UserResolver {
-
-  @Query(() => [User])
-  public async getUsers(): Promise<User[]> {
-    return await userService.findAll();
-  }
-
-  @Query(() => User, {
-    nullable: true,
-  })
-  public async getUser(@Arg("id") id: String): Promise<User | null> {
-    return await userService.findById(id as string);
-  }
-
   @Mutation(() => User)
   public async createUser(@Arg("data") data: CreateUserDTO): Promise<User> {
     return await userService.save(data as UserEntity);
